f675ff6d3ea0b001b9ba55eca7d63de3e7b8455d
akanellop <akanellop42@gmail.com>
2017-03-26 19:57:11 +0300

    main thread xana apo tin arxi grammenh apo mas

--- mainThread.java
+++ mainThread.java
@@ -1,35 +1,40 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
+//import java.util.Date;
+import java.util.*;
+
 
 public class mainThread {
-public static void main(String[] args) throws IOException {
- 
-    int portNumber = (8000);
- 
-    try (
-      ServerSocket serverSocket =
-        new ServerSocket(portNumber);
-      Socket clientSocket = serverSocket.accept();   
-      PrintWriter out =
-        new PrintWriter(clientSocket.getOutputStream(), true);           
-      BufferedReader in = new BufferedReader(
-        new InputStreamReader(clientSocket.getInputStream()));
-    ) {
-      String inputLine;
-	  String outputLine;
-      while ((inputLine = in.readLine()) != null) {
-        out.println(inputLine);
-        System.out.println("echoing: "+inputLine);
-		outputLine="saff";
-		out.println(outputLine);
-		
-		
-      }
-    } catch (IOException e) {
-      System.out.println("Exception caught when trying to listen on port "
-        + portNumber + " or listening for a connection");
-      System.out.println(e.getMessage());
-    }
+public static void main(String[] args) throws IOException{
+	String req;
+	String resp;
+	Scanner inputStream=null;
+	PrintWriter outputStream=null;
+	//pairno port apo terminal
+	int portNumber = Integer.parseInt(args[0]);
+	resp="HTTP/1.1 200 OK";
+	//dimioyrgo server kclient pano sto port- ginetai i sindesi
+	ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+    Socket clientSocket = serverSocket.accept();   
+	
+	try{
+	//dimiourgo ta stremas epikoinonias
+	inputStream =new Scanner(new InputStreamReader(clientSocket.getInputStream()));
+	//to inputStream perimenei na diavazei oti tha steilei o cliennt
+	outputStream= new PrintWriter(new DataOutputStream(clientSocket.getOutputStream()));
+	//to output stream grafei ston client
+	
+	//o server trexei sinexeia kai perimenei requests
+	while(true){
+		//otan vrei request pernaei to minima tou client sto output stream
+		 while ((req = inputStream.nextLine()) != null) {
+			 //outputStream.println(req);
+			 System.out.println("echoing: "+req);
+		 }
+		 outputStream.println(resp);
+	}
+	}
+	catch(Exception e){}
   }
 
 }
