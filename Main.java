88505d7c385c4c89f094b30ceedb69555f882153
akanellop <akanellop42@gmail.com>
2017-03-27 22:20:29 +0300

    main with variety of reference html part

--- Main.java
+++ Main.java
@@ -3,15 +3,15 @@ import java.net.*;
 import java.util.*;
 public class Main{
 	
-	public static void makeResp(String str,PrintWriter pw){
-		System.out.println("in make resp arrrivew:"+str);
+	public static void makeResp(String str,PrintWriter pw) throws Exception{
+		
 		String[] tokens = str.split(" ");
 		String s="";
 		String resp="";
-		String path = "C:\\Users\\Κατερίνα\\Desktop";
-		path+= tokens[1];
+		String path = "C:\\Users\\Κατερίνα\\Desktop\\root.txt";
+		//path+= tokens[1];
 		File f = new File(path);
-		
+		System.out.println(resp);
 		Date today = new Date();
 		resp="HTTP/1.1 404 NOT FOUND\r\n" ;
 		resp += today +"\r\n";
@@ -23,7 +23,20 @@ public class Main{
 		htmlBuilder.append("<head>");
 		htmlBuilder.append("<title>ERROR 404</title>");
 		htmlBuilder.append("<body>");
-		htmlBuilder.append("<p>Error 404 not found</p>");
+		
+		
+		htmlBuilder.append("<a href=\"file:////C:\\Users\\Κατερίνα\\Desktop\\try.txt\">Link 1</a>");
+		//htmlBuilder.append("<a href=\"file:///C/Users/Κατερίνα/Desktop/try.txt\">Link 1</a>");
+		//StringBuilder str_try."<a href=\"file:///C:\\Users\\Κατερίνα\\Desktop\\try.txt\">Link 1</a>";
+		//System.out.println(str_try);
+		//System.out.println(str_try);
+		//htmlBuilder.append(str_try);
+		//htmlBuilder.append("<a href=\"google.com\">this</a>");
+		//htmlBuilder.append("<a href=\"file:///" + path+"\">" + f.getName()+"</a>");
+		//htmlBuilder.append("<a href="+ f.getCanonicalPath()+ ">"+f.getName()+"</a>");
+		
+		
+		
 		htmlBuilder.append("</body>");
 		htmlBuilder.append("</html>");
 		resp=htmlBuilder.toString();
@@ -53,16 +66,21 @@ public class Main{
 		//for getting the input from client (getting request)
 		BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
 		//reading from client
-		String Req="";
-		while ((Req = fromClient.readLine()) != null) {
-			//toClient.println(Req);
-			System.out.println(Req);
-			//use this to send response
-			String[] tokens = Req.split(" ");
-			if(tokens[0].equals("GET")){
-				System.out.println("enters");
-				makeResp(Req,toClient);
+		while(true){
+			String Req="";
+			try{
+				while ((Req = fromClient.readLine()) != null) {
+					//toClient.println(Req);
+					System.out.println(Req);
+					//use this to send response
+					String[] tokens = Req.split(" ");
+					if(tokens[0].equals("GET")){
+						makeResp(Req,toClient);
+						
+					}
+				}
 			}
+			catch(Exception ex){}
 		}
 		
 		
